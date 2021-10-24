621ab1791bebd1f32f0d16837ca128883b7eaefb
alexcodeable <alexokomado@gmail.com>
2021-10-24 20:31:47 +0100

    added result partial for friends
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,7 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [ :friends]
   def index
   end
 
   def pricing
   end
-
-  def friends
-    @friends = current_user.friends
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
   get 'pricing', to: 'home#pricing'
-  get 'friends', to: 'home#friends'
+  get 'friends', to: 'users#friends'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
