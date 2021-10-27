6ee58c2a13f0712d20a97b7f9ada6668192fdea3
alexcodeable <alexokomado@gmail.com>
2021-10-27 13:59:59 +0100

    added mailjet
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'in-v3.mailjet.com',
-    port:                 587,
+    port:                 465,
     domain:               'https://stuck-track.herokuapp.com',
     user_name:             Rails.application.credentials.mailjet[:api_key],
     password:              Rails.application.credentials.mailjet[:secret_key],
