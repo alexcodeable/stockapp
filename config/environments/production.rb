335a8730aff9fb0014e511d82fc5125d2f923cf6
alexcodeable <alexokomado@gmail.com>
2021-10-28 14:16:34 +0100

    updated mailer for production
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -127,5 +127,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'stuck-track.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'stuck-track.herokuapp.com' }
 end
